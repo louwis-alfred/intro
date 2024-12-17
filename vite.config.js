a13c6c39c3b8ef8b145716db9d9c599eefda2806
Somnus <nabayan.louwisalfred.08142002@gmail.com>
2024-12-17 11:02:08 +0800

    Clean up after deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  }
-  
-})
+  },
+  build: {
+    outDir: 'build', // Set the output directory to 'build'
+  },
+  base: '/mini-portfolio/', // Adjust the base path for GitHub Pages
+});
